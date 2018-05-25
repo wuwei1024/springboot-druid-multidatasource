0920c0eccd497de9f4818829a2a2e5c0fc374c51
DESKTOP-QCDKD6Q\JDRJ <1472101076@qq.com>
2018-05-25 16:19:40 +0800

    springboot升级到2.0.2，添加Date类型时间处理配置

--- src/main/java/com/test/Application.java
+++ src/main/java/com/test/Application.java
@@ -1,6 +1,5 @@
 package com.test;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -12,9 +11,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ServletComponentScan
-@MapperScan(basePackages = {"com.test.dao.read","com.test.dao.write"})
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
