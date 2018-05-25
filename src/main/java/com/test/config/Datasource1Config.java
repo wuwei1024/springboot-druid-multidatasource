5ae719f8e8683e20d64726becede0dc57b4dbbda
DESKTOP-QCDKD6Q\JDRJ <1472101076@qq.com>
2018-05-25 12:44:22 +0800

    配置文件读取方式修改为@value

--- src/main/java/com/test/config/Datasource1Config.java
+++ src/main/java/com/test/config/Datasource1Config.java
@@ -6,12 +6,15 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
@@ -21,11 +24,21 @@ import javax.sql.DataSource;
  */
 @Primary
 @Configuration
+@EnableTransactionManagement
 @MapperScan(basePackages = {"com.test.dao.read"}, sqlSessionTemplateRef = "sqlSessionTemplate1")
 public class Datasource1Config {
 
+    @Value("${mybatis.configuration.mapUnderscoreToCamelCase}")
+    private boolean mapUnderscoreToCamelCase;
+
+    @Value("${mybatis.mapperLocations}")
+    private String mapperLocations;
+
+    @Value("${mybatis.typeAliasesPackage}")
+    private String typeAliasesPackage;
+
     /**
-     * 配置数据源
+     * datasource配置
      *
      * @return
      */
@@ -36,6 +49,13 @@ public class Datasource1Config {
         return new DruidDataSource();
     }
 
+    /**
+     * SqlSessionFactory配置
+     *
+     * @param dataSource
+     * @return
+     * @throws Exception
+     */
     @Bean(name = "sqlSessionFactory1")
     public SqlSessionFactory setSqlSessionFactory(@Qualifier("datasource1") DataSource dataSource) throws Exception {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
@@ -43,17 +63,35 @@ public class Datasource1Config {
         bean.setDataSource(dataSource);
         //设置驼峰转换
         org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
-        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setMapUnderscoreToCamelCase(mapUnderscoreToCamelCase);
         bean.setConfiguration(configuration);
         //设置mapper文件位置
-        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
         //给包中的类注册别名，注册后可以直接使用类名，而不用使用全限定的类名
-        bean.setTypeAliasesPackage("com.test.entity");
+        bean.setTypeAliasesPackage(typeAliasesPackage);
         return bean.getObject();
     }
 
+    /**
+     * SqlSessionTemplate配置
+     *
+     * @param sqlSessionFactory
+     * @return
+     */
     @Bean(name = "sqlSessionTemplate1")
     public SqlSessionTemplate setSqlSessionTemplate(@Qualifier("sqlSessionFactory1") SqlSessionFactory sqlSessionFactory) {
         return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    /**
+     * 配置事物管理器
+     *
+     * @return
+     */
+    @Bean(name = "datasource1TransactionManager")
+    public DataSourceTransactionManager clusterTransactionManager(@Qualifier("datasource1") DataSource dataSource) {
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource);
+        return dataSourceTransactionManager;
     }
 }
--- src/main/java/com/test/config/Datasource2Config.java
+++ src/main/java/com/test/config/Datasource2Config.java
@@ -6,11 +6,14 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
@@ -19,9 +22,24 @@ import javax.sql.DataSource;
  * @date 2018/5/3 15:19
  */
 @Configuration
+@EnableTransactionManagement
 @MapperScan(basePackages = {"com.test.dao.write"}, sqlSessionTemplateRef = "sqlSessionTemplate2")
 public class Datasource2Config {
 
+    @Value("${mybatis.configuration.mapUnderscoreToCamelCase}")
+    private boolean mapUnderscoreToCamelCase;
+
+    @Value("${mybatis.mapperLocations}")
+    private String mapperLocations;
+
+    @Value("${mybatis.typeAliasesPackage}")
+    private String typeAliasesPackage;
+
+    /**
+     * datasource配置
+     *
+     * @return
+     */
     @Bean(name = "dataSource2")
     @ConfigurationProperties(prefix = "spring.datasource.ds2")
     public DataSource setDataSource() {
@@ -29,20 +47,49 @@ public class Datasource2Config {
         return new DruidDataSource();
     }
 
+    /**
+     * SqlSessionFactory配置
+     *
+     * @param dataSource
+     * @return
+     * @throws Exception
+     */
     @Bean(name = "sqlSessionFactory2")
     public SqlSessionFactory setSqlSessionFactory(@Qualifier("dataSource2") DataSource dataSource) throws Exception {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+        //设置数据源
         bean.setDataSource(dataSource);
+        //设置驼峰转换
         org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
-        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setMapUnderscoreToCamelCase(mapUnderscoreToCamelCase);
         bean.setConfiguration(configuration);
-        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
-        bean.setTypeAliasesPackage("com.test.entity");
+        //设置mapper文件位置
+        bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
+        //给包中的类注册别名，注册后可以直接使用类名，而不用使用全限定的类名
+        bean.setTypeAliasesPackage(typeAliasesPackage);
         return bean.getObject();
     }
 
+    /**
+     * SqlSessionTemplate配置
+     *
+     * @param sqlSessionFactory
+     * @return
+     */
     @Bean(name = "sqlSessionTemplate2")
     public SqlSessionTemplate setSqlSessionTemplate(@Qualifier("sqlSessionFactory2") SqlSessionFactory sqlSessionFactory) {
         return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    /**
+     * 配置事物管理器
+     *
+     * @return
+     */
+    @Bean(name = "datasource2TransactionManager")
+    public DataSourceTransactionManager clusterTransactionManager(@Qualifier("dataSource2") DataSource dataSource) {
+        DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource);
+        return dataSourceTransactionManager;
     }
 }
